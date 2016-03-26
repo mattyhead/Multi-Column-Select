0d66b2ede2a22685ae08b1ede801e05a84694663
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-26 15:23:54 -0400

    made optioncount module-level so we can have unique IDs

--- Multi-Column-Select/Multi-Column-Select.js
+++ Multi-Column-Select/Multi-Column-Select.js
@@ -5,10 +5,8 @@
  *
  */
 (function($) {
-
     //module varables
     optioncount = 0,
-
     //private functions
     itemclick = function(selector, itemClass, args) {
         var $itemdata = $(selector).attr('data');
@@ -25,15 +23,12 @@
             }
             $menucontainer.siblings('select').val(args);
         }
-
         if (!$menucontainer.hasClass('Multi')) {
             $menucontainer.siblings('select').val($itemdata); //bind form value
             $(selector).siblings('a.' + itemClass).removeClass('active'); //remove all active states
             $(selector).addClass('active'); //add new active state to clicked item
         }
-
     },
-
     init_msc = function(openmenu, opentext, container, multi, append) {
         var toggle = document.createElement('a');
         var mcscontainer = document.createElement('div');
@@ -43,7 +38,6 @@
         }
         $(mcscontainer).addClass(container).appendTo(append);
     },
-
     generateitems = function(selector, useOptionText, idprefix, itemClass, containerClass) {
         var itemtemplate;
         var idtemplate = "";
@@ -59,7 +53,6 @@
         itemtemplate = "<a class='" + itemClass + "' data='" + $(selector).attr('value') + idtemplate + "'>" + settext + "</a>";
         $menucontainer.siblings('.' + containerClass).append(itemtemplate);
     },
-
     destroymsc = function(selector) {
         var $mcs = selector.find('select');
         $mcs.show(); // Shows the Select control if it was hidden;
@@ -68,15 +61,11 @@
             $mcs.next().remove(); // Remove the generated items
         }
     };
-
     $.fn.MultiColumnSelect = function(options) {
-        var args = [];
-        var selected = [];
-        var $optioncount;
-
-        $optioncount = 0;
-
-        var settings = $.extend({
+        var args = [],
+        selected = [],
+        //optioncount = 0,
+        settings = $.extend({
             multiple: false, // Single or Multiple Select- Default Single
             useOptionText: true, // Use text from option. Default true, use false if you plan to use images
             hideselect: true, // Hide Original Select Control
@@ -91,40 +80,27 @@
             onClose: null,
             onItemSelect: null
         }, options);
-
         this.find('select').val(0);
-
         if (settings.hideselect === true) {
             this.find('select').hide();
         } else {
             this.find('select').show();
         }
-
         init_msc(settings.openmenuClass, settings.openmenuText, settings.containerClass, settings.multiple, this); //create the wrapper
-
         this.find('select option').each(function() //get elements in dropdown
             {
-
                 if ($(this).attr('selected')) {
-
                     generateitems(this, settings.useOptionText, settings.idprefix, [settings.itemClass + ' selected'], settings.containerClass);
-
-
                     var $select = $('.selected').parent().prev().prev();
                     if ($select.val() !== null) {
                         args = $select.val();
                     }
                     itemclick($('.selected'), settings.itemClass, args);
                     $('.selected').toggleClass('selected');
-
-
-
                 } else {
                     generateitems(this, settings.useOptionText, settings.idprefix, settings.itemClass, settings.containerClass);
-
                 }
             });
-
         this.on('click', '.' + settings.itemClass, function(e) //on menu item click
             {
                 var $select = $(this).parent().prev().prev();
@@ -137,7 +113,6 @@
                 }
                 e.preventDefault();
             });
-
         this.find('.' + settings.openmenuClass).on('click', function(e) {
             var $menucontainer = $(this).next();
             if ($(this).hasClass(settings.openclass)) {
@@ -162,14 +137,11 @@
         });
         return this;
     };
-
     //public functions
     $.fn.MultiColumnSelectDestroy = function() {
         destroymsc(this);
     };
-
     $.fn.MultiColumnSelectAddItem = function(itemvalue, itemtext, idprefix) {
-
         var $mcs = this.find('select');
         var $count = this.find('select options').size();
         $mcs.append($('<option/>', {
@@ -183,7 +155,6 @@
             var $menuitem = $container.children();
             var $menuitemClass = $menuitem.attr('class');
             $menuitemClass = $menuitemClass.substring(0, $menuitemClass.indexOf(' '));
-
             var idtemplate = "";
             if (typeof(idprefix) !== 'undefined' && idprefix.length()) {
                 idtemplate = "' id='" + idprefix + $count.toString();
